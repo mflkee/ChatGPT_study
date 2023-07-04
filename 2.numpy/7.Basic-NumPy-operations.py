aa4223823e8758d79d57d2a66617d246c2c8185b
mflkee <makeevgleb86rus@gmail.com>
2023-07-04 18:00:01 +0500

    add file

---
+++ 2.numpy/7.Basic-NumPy-operations.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.array([5, 10, 15, 20, 25])
+b = np.array([0, 1, 2, 3, 4])
+
+c = a - b
+print(c)
+
+d = b ** 2
+print(d)
+
+i = 10 * np.sin(a)
+print(i)
+
+f = a < 15
+print(f)
